62677c9ecb3abe8599fdf8448201d8f8bb63fa3a
Ray <ngorui15@gmail.com>
2023-07-18 01:56:49 +0800

    c

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var aText = new Array(
     "",
     "With a strong foundation in programming languages such as JavaScript, Python, and C++, I am always eager to learn more and take on new challenges. Overall, I am a driven and motivated programmer with a passion for coding and a desire to continuously grow and improve my skills."
     );
-    var iSpeed = 80; // time delay of print out
+    var iSpeed = 30; // time delay of print out
     var iIndex = 0; // start printing array at this posision
     var iArrLength = aText[0].length; // the length of the text array
     var iScrollAt = 20; // start scrolling up at this many lines
